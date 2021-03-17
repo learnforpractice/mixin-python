d391b22cc92ffcf26bef5e2b12bd6d56865e8ca0
learnforpractice <learnforpractice@gmail.com>
2021-03-17 08:54:19 +0800

    Update test_testnet.py

--- tests/test_testnet.py
+++ tests/test_testnet.py
@@ -44,7 +44,7 @@ class TestMixinApi(object):
 
     @classmethod
     def teardown_class(cls):
-        cls.testnet.shutdown(cleanup=False)
+        cls.testnet.shutdown(cleanup=True)
 
     def setup_method(self, method):
         logger.warning('test start: %s', method.__name__)
@@ -77,6 +77,13 @@ class TestMixinApi(object):
             'view_key': 'bd1a337f2319d502eb062a433cb79cf8e2daadd6bcd0bb2a21d4b073549cb30c',
             'spend_key': 'bd449970bdb5cf9afaf1f9c574a94f06ff7a0d3af0d9387867e1ba7e9193eb03'
         }
+
+        account2 = {
+            'address': 'XINHJLCRBWJ3AgcrNhKppVMvjinapzumLVL253opKzs6Jk5bUBHWKH6paPr2exhwqYZ3FcPtbnitJMF6TXk8UcEx8u2nUt4S',
+            'view_key': 'fae95f3dfaf0a7b2f4ca95d6ed94f8002492875e018000f786284be1beacf10c',
+            'spend_key': '0ff0016d98026b21df80f4b1fe0db5fc460e7e66f47f67acfd773e5cc4fbb207'
+        }
+        logger.info("++++++++++++++++++deposit+++++++++++++++")
         trx = {
             "asset": "a99c2e0e2b1da4d648755ef19bd95139acbbe6564cfb06dec7cd34931ca72cdc",
             "inputs": [{
@@ -106,8 +113,117 @@ class TestMixinApi(object):
             "inputIndex": "0"
         }
         r = self.api.sign_transaction(params)
-        logger.info(r)
+        # logger.info(r)
         r = await self.api.send_transaction(r['raw'])
-        logger.info(r)
+        # logger.info(r)
+        deposit_hash = r['hash']
 
-        
+        while True:
+            r = await self.api.get_transaction(deposit_hash)
+            if r:
+                break
+            await asyncio.sleep(0.5)
+        # logger.info(r)
+
+        logger.info("++++++++++++++++++transfer+++++++++++++++")
+        trx = {
+            "asset": 'a99c2e0e2b1da4d648755ef19bd95139acbbe6564cfb06dec7cd34931ca72cdc',
+            "inputs": [
+                {
+                "hash": deposit_hash,
+                "index": 0
+                }
+            ],
+            "outputs": [
+                {
+                "amount": "51",
+                "accounts": [account['address']],
+                "script": "fffe01",
+                "type": 0
+                },
+                {
+                "amount": "49",
+                "accounts": [account2['address']],
+                "script": "fffe01",
+                "type": 0
+                }
+            ]
+        }
+
+        # logger.info(trx)
+        params = {
+            "seed": '', #account['spend_key'],
+            "key": json.dumps([account['view_key'] + account['spend_key']]),
+            "raw": json.dumps(trx),
+            "inputIndex": "0"
+        }
+        r = await self.api.get_info()
+
+        await asyncio.sleep(2.0)
+        for i in range(10):
+            try:
+                r = self.api.sign_transaction(params)
+                # logger.info(r)
+                transfer_ret = await self.api.send_transaction(r['raw'])
+                break
+            except Exception as e:
+                logger.info(e)
+                await asyncio.sleep(1.0)
+
+        else:
+            raise Exception('transfer test failed!')
+        # logger.info(transfer_ret)
+
+        logger.info("++++++++++++++++++withdraw+++++++++++++++")
+        trx = {
+            "asset": "a99c2e0e2b1da4d648755ef19bd95139acbbe6564cfb06dec7cd34931ca72cdc",
+            "extra": b"hello,world".hex(),
+            "inputs": [
+                {
+                "hash": transfer_ret['hash'],
+                "index": 0
+                }
+            ],
+            "outputs": [
+                {
+                "amount": "50",
+                "withdrawal": {
+                    "chain": "8dd50817c082cdcdd6f167514928767a4b52426997bd6d4930eca101c5ff8a27",
+                    "asset_key": "0xa974c709cfb4566686553a20790685a47aceaa33",
+                    "address": "0x62E2110D4F76e15Ae40FE1e139f249a1F1afd8b5",
+                    "tag":"hello,world"
+                },
+                "type": 161
+                },
+                {
+                "amount": "1",
+                "accounts": [
+                    account2['address']
+                ],
+                "script": "fffe01",
+                "type": 0
+                }
+            ]
+        }
+
+        params = {
+            "seed": '', #account['spend_key'],
+            "key": json.dumps([account['view_key'] + account['spend_key']]),
+            "raw": json.dumps(trx),
+            "inputIndex": "0"
+        }
+
+        await asyncio.sleep(2.0)
+        for i in range(10):
+            try:
+                r = self.api.sign_transaction(params)
+                # logger.info(r)
+                transfer_ret = await self.api.send_transaction(r['raw'])
+                break
+            except Exception as e:
+                logger.info(e)
+                await asyncio.sleep(1.0)
+
+
+
+
