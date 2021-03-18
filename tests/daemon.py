f570ca9960dc97d469742b6c4ff2b9cad06f0beb
learnforpractice <learnforpractice@gmail.com>
2021-03-18 21:49:53 +0800

    Add daemon.py

---
+++ tests/daemon.py
@@ -0,0 +1,22 @@
+import time
+import shlex
+import subprocess
+from mixin import log
+
+logger = log.get_logger(__name__)
+logger.addHandler(log.handler)
+
+cmd = 'python3.7 -m mixin.main kernel -dir ../mixin/config'
+#cmd = 'ls -l'
+
+log = open(f'mixin.log', 'a')
+
+while True:
+    args = shlex.split(cmd)
+    try:
+        p = subprocess.Popen(args, stdout=log, stderr=log)
+        r = p.wait()
+        logger.warning('%s return %s', cmd, r)
+        time.sleep(1.0)
+    except Exception as e:
+        logger.warning(e)
