6ef5f61031cfeed2769582079635a21638622dc9
learnforpractice <learnforpractice@gmail.com>
2021-04-11 01:27:20 +0800

    Update install command

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-python3 -m pip uninstall mixin -y;python3 -m pip install dist/mixin-0.1.0-cp37-cp37m-macosx_10_9_x86_64.whl 
+python3 -m pip uninstall mixin -y;python3 -m pip install dist/mixin-0.1.0-cp37-cp37*.whl
 
