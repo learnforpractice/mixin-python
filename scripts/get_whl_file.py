16466c10cc739f4a3d3b7b5181dbba49b9073d6f
learnforpractice <learnforpractice@gmail.com>
2021-10-10 18:04:31 +0800

    Update get_whl_file.py

--- scripts/get_whl_file.py
+++ scripts/get_whl_file.py
@@ -1,5 +1,18 @@
 import os
-for f in os.listdir('./dist'):
+import sys
+
+search_dir = 'dist'
+keyword = ''
+
+if len(sys.argv) >= 2:
+    search_dir = sys.argv[1]
+
+if len(sys.argv) >= 3:
+    keyword = sys.argv[2]
+
+for f in os.listdir(search_dir):
     if f.endswith('.whl'):
+        if keyword and not keyword in f:
+            continue
         print(f)
         break
