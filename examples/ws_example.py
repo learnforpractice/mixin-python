885e98c1ea392002541eb2d7b037212ae99b3c7d
learnforpractice <learnforpractice@gmail.com>
2022-12-14 16:24:13 +0800

    Update ws_example.py

--- examples/ws_example.py
+++ examples/ws_example.py
@@ -8,68 +8,69 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(module)s %(lineno)d %(message)s')
 logger = logging.getLogger(__name__)
 
-from pymixin.mixin_bot_api import MixinBotApi
 from pymixin.mixin_ws_api import MixinWSApi
 
+bot_config = None
 
-bot_config = sys.argv[1]
-
-with open(bot_config) as f:
+with open(sys.argv[1]) as f:
     bot_config = f.read()
     bot_config = json.loads(bot_config)
-    mixin_bot = MixinBotApi(bot_config)
 
-async def on_message(message):
-    logger.info(message)
-    if 'error' in message:
-        return
-    action = message["action"]
-    if action not in ["ACKNOWLEDGE_MESSAGE_RECEIPT", "CREATE_MESSAGE", "LIST_PENDING_MESSAGES"]:
-        logger.info("unknow action %s", action)
-        return
+class MixinBot(MixinWSApi):
+    def __init__(self):
+        super().__init__(bot_config, on_message=self.on_message)
 
-    if action == "ACKNOWLEDGE_MESSAGE_RECEIPT":
-        return
+    async def on_message(self, message):
+        logger.info(message)
+        if 'error' in message:
+            return
+        action = message["action"]
+        if action not in ["ACKNOWLEDGE_MESSAGE_RECEIPT", "CREATE_MESSAGE", "LIST_PENDING_MESSAGES"]:
+            logger.info("unknow action %s", action)
+            return
 
-    if not action == "CREATE_MESSAGE":
-        return
+        if action == "ACKNOWLEDGE_MESSAGE_RECEIPT":
+            return
 
-    data = message["data"]
-    msgid = data["message_id"]
-    typeindata = data["type"]
-    categoryindata = data["category"]
-    user_id = data["user_id"]
-    conversation_id = data["conversation_id"]
+        if not action == "CREATE_MESSAGE":
+            return
 
-    created_at = data["created_at"]
-    updated_at = data["updated_at"]
+        data = message["data"]
+        msgid = data["message_id"]
+        typeindata = data["type"]
+        categoryindata = data["category"]
+        user_id = data["user_id"]
+        conversation_id = data["conversation_id"]
 
-    await mixin_ws.echoMessage(msgid)
+        created_at = data["created_at"]
+        updated_at = data["updated_at"]
 
-    logger.info('user_id %s', user_id)
-    logger.info("created_at %s",created_at)
+        await self.echoMessage(msgid)
 
-    if 'error' in message:
-        return
+        logger.info('user_id %s', user_id)
+        logger.info("created_at %s",created_at)
 
-    if not categoryindata in ["SYSTEM_ACCOUNT_SNAPSHOT", "PLAIN_TEXT", "SYSTEM_CONVERSATION", "PLAIN_STICKER", "PLAIN_IMAGE", "PLAIN_CONTACT"]:
-        logger.info("unknow category:", categoryindata)
-        return
+        if 'error' in message:
+            return
 
-    if not categoryindata == "PLAIN_TEXT" and typeindata == "message":
-        return
+        if not categoryindata in ["SYSTEM_ACCOUNT_SNAPSHOT", "PLAIN_TEXT", "SYSTEM_CONVERSATION", "PLAIN_STICKER", "PLAIN_IMAGE", "PLAIN_CONTACT"]:
+            logger.info("unknown category: %s", categoryindata)
+            return
 
-    data = data["data"]
-    logger.info(data)
-    data = base64.b64decode(data)
-    logger.info('++++on_message:cmd %s', data)
-    data = base64.urlsafe_b64decode(data+b'===')
-    logger.info(data)
+        if not categoryindata == "PLAIN_TEXT" and typeindata == "message":
+            return
 
-mixin_ws = MixinWSApi(bot_config, on_message=on_message)
+        data = data["data"]
+        logger.info(data)
+        data = base64.urlsafe_b64decode(data)
+        logger.info(data)
+        data = data.decode()
+        await self.sendUserText(conversation_id, user_id, data)
+
+bot = MixinBot()
 
 async def start():
-    await mixin_ws.run()
+    await bot.run()
 
 if __name__ == '__main__':
     asyncio.run(start())
