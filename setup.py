b68b17d60f475f51b2940ddbb06b71578126dc31
learnforpractice <learnforpractice@gmail.com>
2023-06-26 19:27:59 +0800

    Remove scikit-build

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import os
 import sys
 import platform
-from setuptools import find_packages
-from skbuild import setup
-    
+
+from setuptools import find_packages, setup, Extension
+from Cython.Build import cythonize
+
+os.environ["CC"] = "clang"
+os.environ["CXX"] = "clang++"
+
 # Require pytest-runner only when running tests
 pytest_runner = (['pytest-runner>=2.0,<3dev']
                  if any(arg in sys.argv for arg in ('pytest', 'test'))
@@ -22,6 +26,48 @@ if platform.system() == 'Windows':
 version = platform.python_version_tuple()
 version = '%s.%s' % (version[0], version[1])
 
+dir_name = os.path.dirname(os.path.realpath(__file__))
+
+root_path = os.path.join(dir_name, 'src/mixin')
+def check_modification():   
+    lib_name = os.path.join(root_path, 'libmixin.a')
+    if not os.path.exists(lib_name):
+        return True
+    modify_time = os.path.getmtime(lib_name)
+    for root, dirs, files in os.walk(root_path):
+        for f in files:
+            if f[-3:] == '.go':
+                f = os.path.join(root, f)
+                file_time = os.path.getmtime(f)
+                if modify_time < file_time:
+                    return True
+    return False
+
+r = check_modification()
+if r:
+    print('mixin lib need to rebuild.')
+    os.system(f'touch {root_path}/main.go')
+
+if platform.system() == 'Windows':
+    os.system('cd ./src/mixin &&go build -o mixin.dll -buildmode=c-shared && copy mixin.dll ../../pysrc/mixin.dll && gendef mixin.dll && lib /def:mixin.def /machine:x64 /out:mixin.lib')
+else:
+    os.system('cd ./src/mixin;go build -o libmixin.a -buildmode=c-archive')
+
+ext_modules = [
+    Extension(
+        'pymixin._mixin',
+        sources=[
+            'src/_mixin.pyx',
+        ],
+        include_dirs=[
+            'src/mixin',
+        ],
+        language='c++',
+        extra_compile_args=['-std=c++17'],
+        extra_link_args=['-L./src/mixin', '-lmixin'],
+    )
+]
+
 setup(
     name="mixin-python",
     version="0.2.10",
@@ -34,6 +80,10 @@ setup(
     package_data={'pymixin': data},
     data_files = data_files,
     scripts=[],
+    ext_modules=cythonize(
+        ext_modules,
+        compiler_directives={'language_level': 3, },
+    ),
     install_requires=[
         "PyJWT>=2.4.0",
         "websockets>=9.1",
